e418beff86c5409fde3f00c0c9fa0daeaaefb34e
Yuk, Yongsu <ysyukr@gmail.com>
2019-12-30 12:37:07 +0900

    Update outdated-chk.sh

--- outdated-chk.sh
+++ outdated-chk.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
+RunScript(){
+    cd website
+    rm -rf ./outdated-ko.txt
+    rm -rf ./outdated-en.txt
+    rm -rf ./docs-ko.txt
+
+    echo -e "Previous branch name: "
+    read prebranch
+    echo "Entered branch name: $prebranch"
+
+    echo -e "Current branch name: "
+    read currbranch
+    echo "Entered branch name: $currbranch"
+
+    echo "Check for differences between branches with the git diff command."
+
+    git diff --name-only upstream/"$prebranch" upstream/"$currbranch" -- content/en/docs >> outdated-en.txt
+
+    find content/ko/docs >> docs-ko.txt
+
+    sed -e 's/content\/ko/content\/en/g' docs-ko.txt > docs-en.txt.tmp
+
+    mv docs-en.txt.tmp docs-en.txt
+
+    comm -12 outdated-en.txt docs-en.txt >> ../outdated-ko.txt
+
+    rm -rf ./outdated-en.txt
+    rm -rf ./docs-ko.txt
+
+    echo "Check the contents of the outdated-ko.txt file."
+
+    cat ../outdated-ko.txt
+}
+
 echo "This script is for checking outdated files of Korean team."
-echo "This script must be run outside the website folder."
 
-cd website
-rm -rf ./outdated-ko.txt
-rm -rf ./outdated-en.txt
-rm -rf ./docs-ko.txt
-
-echo -e "Previous branch name: "
-read prebranch
-echo "Entered branch name: $prebranch"
-
-echo -e "Current branch name: "
-read currbranch
-echo "Entered branch name: $currbranch"
-
-echo "Check for differences between branches with the git diff command."
-
-git diff --name-only upstream/"$prebranch" upstream/"$currbranch" -- content/en/docs >> outdated-en.txt
-
-find content/ko/docs >> docs-ko.txt
-
-sed -e 's/content\/ko/content\/en/g' docs-ko.txt > docs-en.txt.tmp
-
-mv docs-en.txt.tmp docs-en.txt
-
-comm -12 outdated-en.txt docs-en.txt >> ../outdated-ko.txt
-
-rm -rf ./outdated-en.txt
-rm -rf ./docs-ko.txt
-
-echo "Check the contents of the outdated-ko.txt file."
-
-cat ../outdated-ko.txt
+if [ -d "website" ]; then
+    echo "Checked the website folder. Run the script."
+    RunScript
+else
+    echo "This script must be run outside the website folder."
+fi
